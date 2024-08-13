873371300bebfac10c94e2d3b4dffd3738c9053c
fuzo0701 <fuzo0701@naver.com>
2024-08-13 09:46:39 +0900

    init

---
+++ src/main/java/kr/ccos/springcmwithslack/SpringCmWithSlackApplication.java
@@ -0,0 +1,13 @@
+package kr.ccos.springcmwithslack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCmWithSlackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCmWithSlackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ccos/springcmwithslack/SpringCmWithSlackApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ccos.springcmwithslack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCmWithSlackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
